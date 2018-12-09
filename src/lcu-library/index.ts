4f77fc15c5b599b862db20180a811207a58f9916
Michael Gearhardt <michael.gearhardt@fathym.com>
2018-12-09 09:50:00 -0700

    Initial checkin

---
+++ src/lcu-library/index.ts
@@ -0,0 +1,51 @@
+import { getWorkspace } from '@schematics/angular/utility/config';
+import { buildDefaultPath } from '@schematics/angular/utility/project';
+import { parseName } from '@schematics/angular/utility/parse-name';
+import { Rule, SchematicContext, Tree, apply, url, noop, filter, move, MergeStrategy, mergeWith, template } from '@angular-devkit/schematics';
+import { ProjectType, WorkspaceProject } from '@schematics/angular/utility/workspace-models';
+import { normalize, strings } from '@angular-devkit/core';
+
+// You don't have to export the function as default. You can also have more than one rule factory
+// per file.
+export function lcuLibrary(options: any): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    setupOptions(tree, options);
+
+    const movePath = (options.flat) ?
+      normalize(options.path) :
+      normalize(options.path + '/' + strings.dasherize(options.name));
+
+    const templateSource = apply(url('./files'), [
+      options.spec ? noop() : filter(path => !path.endsWith('.spec.ts')),
+      template({
+        ...strings,
+        ...options,
+      }),
+      move(movePath),
+    ]);
+
+    const rule = mergeWith(templateSource, MergeStrategy.Default);
+
+    return rule(tree, _context);
+  };
+}
+
+export function setupOptions(host: Tree, options: any): Tree {
+  const workspace = getWorkspace(host);
+
+  if (!options.project)
+    options.project = workspace.defaultProject || Object.keys(workspace.projects)[0];
+
+  const project = workspace.projects[options.project];
+
+  if (options.path === undefined && project.projectType == ProjectType.Application)
+    options.path = buildDefaultPath(<WorkspaceProject<ProjectType.Application>>project);
+
+  const parsedPath = parseName(options.path, options.name);
+
+  options.name = parsedPath.name;
+
+  options.path = parsedPath.path;
+
+  return host;
+}
