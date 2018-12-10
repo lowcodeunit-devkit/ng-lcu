67b491aac66c14338f3bad1c90bc7ec35493b2a6
Michael Gearhardt <michael.gearhardt@fathym.com>
2018-12-10 08:39:09 -0700

    Library root

--- src/library/index.ts
+++ src/library/index.ts
@@ -1,51 +1,41 @@
 import { getWorkspace } from '@schematics/angular/utility/config';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 import { buildDefaultPath } from '@schematics/angular/utility/project';
 import { parseName } from '@schematics/angular/utility/parse-name';
-import { Rule, SchematicContext, Tree, apply, url, noop, filter, move, MergeStrategy, mergeWith, template } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, apply, url, noop, filter, move, MergeStrategy, mergeWith, template, chain, externalSchematic } from '@angular-devkit/schematics';
 import { ProjectType, WorkspaceProject } from '@schematics/angular/utility/workspace-models';
 import { normalize, strings } from '@angular-devkit/core';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function library(options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
+  return (tree: Tree, context: SchematicContext) => {
     setupOptions(tree, options);
 
-    const movePath = (options.flat) ?
-      normalize(options.path) :
-      normalize(options.path + '/' + strings.dasherize(options.name));
-
-    const templateSource = apply(url('./files'), [
-      options.spec ? noop() : filter(path => !path.endsWith('.spec.ts')),
-      template({
-        ...strings,
-        ...options,
-      }),
-      move(movePath),
+    const rule = chain([
+      externalSchematic('@schematics/angular', 'library', {
+        entryFile: 'lcu_api',
+        prefix: options.prefix,
+        skipInstall: true
+      })
     ]);
 
-    const rule = mergeWith(templateSource, MergeStrategy.Default);
-
-    return rule(tree, _context);
+    if (options.skipInstall)
+      context.addTask(new NodePackageInstallTask());
+    //  TODO:  Install NPM Packages
+    
+    return rule(tree, context);
   };
 }
 
 export function setupOptions(host: Tree, options: any): Tree {
   const workspace = getWorkspace(host);
 
-  if (!options.project)
-    options.project = workspace.defaultProject || Object.keys(workspace.projects)[0];
+  options.name = options.name || 'library';
 
-  const project = workspace.projects[options.project];
+  options.prefix = options.prefix || 'lib';
 
-  if (options.path === undefined && project.projectType == ProjectType.Application)
-    options.path = buildDefaultPath(<WorkspaceProject<ProjectType.Application>>project);
-
-  const parsedPath = parseName(options.path, options.name);
-
-  options.name = parsedPath.name;
-
-  options.path = parsedPath.path;
+  options.skipInstall = options.skipInstall || false;
 
   return host;
 }
