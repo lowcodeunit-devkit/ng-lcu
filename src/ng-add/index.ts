c1f128ed00104df8c0583a68ca1955b56546172a
Michael Gearhardt <michael.gearhardt@fathym.com>
2018-12-09 23:28:19 -0700

    try this

--- src/ng-add/index.ts
+++ src/ng-add/index.ts
@@ -3,12 +3,8 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function ngAdd(options: any): Rule {
-  return (tree: Tree, context: SchematicContext) => {
-    tree.create(options.name || 'lcu-test.json', 'hey');
-
-    context.toString();
-    
+export function ngAdd(_options: any): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
     return tree;
   };
 }
