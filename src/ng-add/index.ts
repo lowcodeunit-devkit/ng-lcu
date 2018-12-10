7a20c3f49d64cb2e8be46ccc8b0694a977bd8375
Michael Gearhardt <michael.gearhardt@fathym.com>
2018-12-10 00:43:21 -0700

    Simple add of lcu.json file

--- src/ng-add/index.ts
+++ src/ng-add/index.ts
@@ -1,10 +1,27 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { getWorkspace } from '@schematics/angular/utility/config';
+import { buildDefaultPath } from '@schematics/angular/utility/project';
+import { parseName } from '@schematics/angular/utility/parse-name';
+import { Rule, SchematicContext, Tree, apply, url, noop, filter, move, MergeStrategy, mergeWith, template } from '@angular-devkit/schematics';
+import { ProjectType, WorkspaceProject } from '@schematics/angular/utility/workspace-models';
+import { normalize, strings, Path } from '@angular-devkit/core';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function ngAdd(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+export function ngAdd(options: any): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    options.repository = options.repository || options.repo;
+    
+    const templateSource = apply(url('./files/project'), [
+      template({
+        ...strings,
+        ...options,
+      }),
+      move('./'),
+    ]);
+
+    const rule = mergeWith(templateSource, MergeStrategy.Overwrite);
+
+    return rule(tree, context);
   };
 }
