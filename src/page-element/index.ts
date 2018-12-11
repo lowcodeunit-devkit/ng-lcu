caf455e15af7f48e7e0d2eb794ffe1652f474e28
Michael Gearhardt <michael.gearhardt@fathym.com>
2018-12-10 21:13:03 -0700

    Checkpoint on Page Element, gotta go break everything into a series of open source projects

--- src/page-element/index.ts
+++ src/page-element/index.ts
@@ -1,10 +1,32 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { getWorkspace } from '@schematics/angular/utility/config';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import { buildDefaultPath } from '@schematics/angular/utility/project';
+import { parseName } from '@schematics/angular/utility/parse-name';
+import { Rule, SchematicContext, Tree, apply, url, noop, filter, move, MergeStrategy, mergeWith, template, chain, externalSchematic } from '@angular-devkit/schematics';
+import { ProjectType, WorkspaceProject } from '@schematics/angular/utility/workspace-models';
+import { normalize, strings, Path, join } from '@angular-devkit/core';
+
+export function pageElement(options: any): Rule {
+  return (host: Tree, context: SchematicContext) => {
+    setupOptions(host, options);
 
 
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
-export function pageElement(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+
+    return host;
   };
+}
+
+export function setupOptions(host: Tree, options: any): Tree {
+  const workspace = getWorkspace(host);
+
+  options.project = options.project ? workspace.projects[options.project] :
+    workspace.defaultProject ? workspace.projects[<string>workspace.defaultProject] : workspace.projects[0];
+
+  options.spec = options.spec || false;
+
+  options.prefix = options.prefix || 'lib';
+
+  options.skipInstall = options.skipInstall || false;
+
+  return host;
 }
