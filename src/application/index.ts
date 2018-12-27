5a0941cbbd33add2c50eb2a9003b5195a9616bed
Michael Gearhardt <michael.gearhardt@fathym.com>
2018-12-27 00:19:07 -0700

    Another adjust to handle angular asset deployment with package.json

--- src/application/index.ts
+++ src/application/index.ts
@@ -1,31 +1,48 @@
-import { getWorkspace } from '@schematics/angular/utility/config';
-import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
-import { buildDefaultPath } from '@schematics/angular/utility/project';
-import { parseName } from '@schematics/angular/utility/parse-name';
-import { Rule, SchematicContext, Tree, apply, url, noop, filter, move, MergeStrategy, mergeWith, template, chain, externalSchematic } from '@angular-devkit/schematics';
-import { ProjectType, WorkspaceProject } from '@schematics/angular/utility/workspace-models';
-import { normalize, strings, Path, join } from '@angular-devkit/core';
-import { addDeployScriptsToPackageFile } from '../utils/helpers';
-import { Logger } from '@angular-devkit/core/src/logger';
+import { getWorkspace } from "@schematics/angular/utility/config";
+import { NodePackageInstallTask } from "@angular-devkit/schematics/tasks";
+import { buildDefaultPath } from "@schematics/angular/utility/project";
+import { parseName } from "@schematics/angular/utility/parse-name";
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  apply,
+  url,
+  noop,
+  filter,
+  move,
+  MergeStrategy,
+  mergeWith,
+  template,
+  chain,
+  externalSchematic
+} from "@angular-devkit/schematics";
+import {
+  ProjectType,
+  WorkspaceProject
+} from "@schematics/angular/utility/workspace-models";
+import { normalize, strings, Path, join } from "@angular-devkit/core";
+import { addDeployScriptsToPackageFile } from "../utils/helpers";
+import { Logger } from "@angular-devkit/core/src/logger";
 
 export function application(options: any): Rule {
   return (host: Tree, context: SchematicContext) => {
     setupOptions(host, options);
 
     const rule = chain([
-      externalSchematic('@schematics/angular', 'application', {
+      externalSchematic("@schematics/angular", "application", {
         name: options.name,
         routing: true,
         prefix: options.prefix,
-        style: 'scss'
+        style: "scss"
       }),
       // processInitWith(options, context),
       addScripts(options),
-      manageDeployAllScript(options)
+      manageDeployAllScript(options),
+      manageAppAssets(options)
     ]);
 
-    if (!options.skipInstall)
-      context.addTask(new NodePackageInstallTask());
+    if (!options.skipInstall) context.addTask(new NodePackageInstallTask());
 
     return rule(host, context);
   };
@@ -43,8 +60,34 @@ export function addScripts(options: any) {
       {
         key: `deploy:${projectSafeName}`,
         value: `ng build ${projectSafeName} --prod && npm publish ./dist/${projectSafeName} --access public`
-      },
+      }
     ]);
+
+    return host;
+  };
+}
+
+export function manageAppAssets(options: any) {
+  return (host: Tree) => {
+    var projectSafeName = strings.dasherize(options.name);
+
+    var packageGlob = {
+      glob: "package.json",
+      input: "./",
+      output: "/"
+    };
+
+    var angularFile = host.get("angular.json");
+
+    var angularJson = angularFile
+      ? JSON.parse(angularFile.content.toString("utf8"))
+      : {};
+
+    angularJson.projects[projectSafeName].architect.build.options.assets.push(
+      packageGlob
+    );
+
+    host.overwrite("angular.json", JSON.stringify(angularJson, null, "\t"));
 
     return host;
   };
@@ -56,20 +99,20 @@ export function manageDeployAllScript(options: any) {
 
     var deployProj = `npm run deploy:${projectSafeName}`;
 
-    var packageFile = host.get('package.json');
+    var packageFile = host.get("package.json");
 
-    var packageJson = packageFile ? JSON.parse(packageFile.content.toString('utf8')) : {};
+    var packageJson = packageFile
+      ? JSON.parse(packageFile.content.toString("utf8"))
+      : {};
 
-    var deployAll = packageJson.scripts['deploy:all'];
+    var deployAll = packageJson.scripts["deploy:all"];
 
-    if (deployAll)
-      deployAll += ` && ${deployProj}`;
-    else
-      deployAll = deployProj;
+    if (deployAll) deployAll += ` && ${deployProj}`;
+    else deployAll = deployProj;
 
-    packageJson.scripts['deploy:all'] = deployAll;
+    packageJson.scripts["deploy:all"] = deployAll;
 
-    host.overwrite('package.json', JSON.stringify(packageJson, null, '\t'));
+    host.overwrite("package.json", JSON.stringify(packageJson, null, "\t"));
 
     return host;
   };
@@ -83,16 +126,16 @@ function blankOutLibrary(options: any, context: SchematicContext) {
 
     var project = workspace.projects[projectName];
 
-    var srcRoot = join(project.root as Path, 'src');
+    var srcRoot = join(project.root as Path, "src");
 
-    var libRoot = join(srcRoot, 'lib');
+    var libRoot = join(srcRoot, "lib");
 
     [
       `${projectName}.component.spec.ts`,
       `${projectName}.component.ts`,
       `${projectName}.module.ts`,
       `${projectName}.service.spec.ts`,
-      `${projectName}.service.ts`,
+      `${projectName}.service.ts`
     ].forEach(filename => {
       var filePath = join(libRoot, filename);
 
@@ -103,7 +146,7 @@ function blankOutLibrary(options: any, context: SchematicContext) {
 
     var lcuApi = join(srcRoot, `${options.entryFile}.ts`);
 
-    host.overwrite(lcuApi, '');
+    host.overwrite(lcuApi, "");
 
     return host;
   };
@@ -168,13 +211,13 @@ function blankOutLibrary(options: any, context: SchematicContext) {
 // }
 
 export function setupOptions(host: Tree, options: any): Tree {
-  options.entryFile = 'lcu.api';
+  options.entryFile = "lcu.api";
 
-  options.initWith = options.initWith || 'Default';
+  options.initWith = options.initWith || "Default";
 
-  options.name = options.name || 'library';
+  options.name = options.name || "library";
 
-  options.prefix = options.prefix || 'lib';
+  options.prefix = options.prefix || "lib";
 
   options.skipInstall = options.skipInstall || false;
 
