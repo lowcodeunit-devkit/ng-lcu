b43246d98673c76cba3636653b1588ee4cd65e26
Michael Gearhardt <michael.gearhardt@fathym.com>
2019-01-11 09:36:43 -0700

    Adjusted so it builds

--- src/forge/files/default/svc/solutions.service.ts
+++ src/forge/files/default/svc/solutions.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { BaseSolutionsService } from '@lcu/solutions';
 import { SolutionModuleConfig } from '@lcu/elements';
-import { __sln__DisplayModule } from '@the-scope/lcu-sln-the-solution';
-import { __el__DisplayModule } from '@the-scope/lcu-el-the-element';
+// import { __sln__DisplayModule } from '@the-scope/lcu-sln-the-solution';
+// import { __el__DisplayModule } from '@the-scope/lcu-el-the-element';
 
 @Injectable({
 	providedIn: 'root'
@@ -22,27 +22,27 @@ export class <%= classify(name) %>SolutionsService extends BaseSolutionsService 
         Icon: 'fullscreen',
         BaseKey: 'forge-solution',
         Modules: <SolutionModuleConfig[]>[
-          {
-            Name: '__the-solution-name__',
-            Control: { Base: 'forge-solution', Details: {}, Type: 'the-solution-type' },
-            Solution: __sln__DisplayModule,
-            DisplaySetups: [
-              {
-                Name: 'Weather Cloud',
-                Icon: 'insert_chart',
-                BaseKey: 'forge-weather-cloud',
-                Modules: [
-                  {
-                    Name: '__the-element-name__',
-                    Control: { Base: 'forge-weather-cloud', Details: { Elements: [], Configs: [] }, Type: 'element-name' },
-                    Display: __el__DisplayModule,
-                    HideDrag: false,
-                    BuilderState: 'Render',
-                  },
-                ]
-              },
-            ],
-          },
+          // {
+          //   Name: '__the-solution-name__',
+          //   Control: { Base: 'forge-solution', Details: {}, Type: 'the-solution-type' },
+          //   Solution: __sln__DisplayModule,
+          //   DisplaySetups: [
+          //     {
+          //       Name: '__the-solution-name__',
+          //       Icon: 'insert_chart',
+          //       BaseKey: '__sln-key__',
+          //       Modules: [
+          //         {
+          //           Name: '__the-element-name__',
+          //           Control: { Base: '__sln-key__', Details: { Elements: [], Configs: [] }, Type: 'element-name' },
+          //           Display: __el__DisplayModule,
+          //           HideDrag: false,
+          //           BuilderState: 'Render',
+          //         },
+          //       ]
+          //     },
+          //   ],
+          // },
         ],
       },
 		];
