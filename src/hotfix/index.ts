c95d221d4e56605e6970cef7094975adabcba548
mcgear <michael.gearhardt@fathym.com>
2019-06-09 00:59:42 -0600

    Fix

--- src/hotfix/index.ts
+++ src/hotfix/index.ts
@@ -29,12 +29,10 @@ export function updateTsConfig(context: SchematicContext, options: any) {
     pathKeys.forEach(pathKey => {
       context.logger.info(pathKey);
   
-      if (pathKey == options.name || `${pathKey}/*` == options.name) {
+      if (pathKey == options.name || pathKey == `${options.name}/*`) {
         var newPath = pathKey.replace(options.name, `${options.scope}/${options.workspace}-${options.name}`);
 
-        if (options.name.endsWith('/*')) {
-          newPath += '/*';
-        }
+        context.logger.info(newPath);
 
         tsConfigJson.compilerOptions.paths[newPath] = tsConfigJson.compilerOptions.paths[pathKey];
 
