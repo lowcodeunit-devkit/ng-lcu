1579adab56c721c010fcf37441ec5796052df643
xchanin <me@shannonbruns.com>
2019-06-26 21:34:56 -0600

    add themes to default app

--- src/lcu-core-app/files/src/app/controls/side-nav/side-nav.component.ts
+++ src/lcu-core-app/files/src/app/controls/side-nav/side-nav.component.ts
@@ -5,12 +5,16 @@ import { map } from 'rxjs/operators';
 import { NavLinkModel } from '../../models/nav-link.model';
 import { UserModel } from '../../models/user.model';
 import { UsersService } from '../../services/user.service';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
+import { SharedNotificationService } from '../../services/shared-notification.service';
+import { ThemeModel } from '../../models/theme.model';
 
 @Component({
   selector: 'lcu-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.scss']
 })
+
 export class SideNavComponent {
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
@@ -20,7 +24,18 @@ export class SideNavComponent {
 
   protected _navLinks: Array<NavLinkModel>;
 
+  public Form: FormGroup;
+
+  public Themes: Array<ThemeModel>;
+
   public Users: Array<UserModel>;
+
+  /**
+   * Access terms field
+   */
+  public get ThemesControl(): AbstractControl {
+    return this.Form.get('themesControl');
+  }
 
   /**
    * Input property for logo
@@ -43,12 +58,46 @@ export class SideNavComponent {
   @Input('nav-links')
   public NavLinks: Array<NavLinkModel>;
 
-  constructor(protected userService: UsersService, protected breakpointObserver: BreakpointObserver) {}
+  constructor(
+    protected userService: UsersService,
+    protected breakpointObserver: BreakpointObserver,
+    protected sharedNotificationService: SharedNotificationService) {}
 
   public ngOnInit(): void {
     this.userService.GetUsers().subscribe((data: Array<UserModel>) => {
       this.Users = data;
     });
+
+    this.setupForm();
+
+    this.setThemes();
+  }
+
+  /**
+   * Change theme
+   * 
+   * @param evt theme type
+   */
+  public SelectTheme(evt: string): void {
+    this.sharedNotificationService.ChangeTheme(evt);
+  }
+
+  protected setupForm(): void {
+    this.Form = new FormGroup({
+      themesControl: new FormControl('arctic-theme')
+    });
+  }
+
+  protected setThemes(): void {
+    this.Themes = [
+      { Color: 'primary-arctic', Icon: 'fiber_manual_record', Label: 'Arctic Theme', Value: 'arctic-theme' },
+      { Color: '', Icon: 'fiber_manual_record', Label: 'Contrast Theme', Value: 'contrast-theme' },
+      { Color: '', Icon: 'fiber_manual_record', Label: 'Cool Candy Theme', Value: 'cool-candy-theme' },
+      { Color: '', Icon: 'fiber_manual_record', Label: 'Flipper Theme', Value: 'flipper-theme' },
+      { Color: '', Icon: 'fiber_manual_record', Label: 'Ice Theme', Value: 'ice-theme' },
+      { Color: '', Icon: 'fiber_manual_record', Label: 'Sea Green Theme', Value: 'sea-green-theme' },
+      { Color: '', Icon: 'fiber_manual_record', Label: 'White Mint Theme', Value: 'white-mint-theme' }
+  ];
   }
 }
 
