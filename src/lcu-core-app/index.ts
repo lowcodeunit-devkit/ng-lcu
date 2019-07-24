8938b3da0dfa64c468486ab731024f0c57a87db2
mcgear <michael.gearhardt@fathym.com>
2019-07-23 23:06:22 -0600

    Lcu Core app test

--- src/lcu-core-app/index.ts
+++ src/lcu-core-app/index.ts
@@ -1,10 +1,24 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, apply, url, move, MergeStrategy, mergeWith, template, chain } from '@angular-devkit/schematics';
+import { strings } from '@angular-devkit/core';
+import { addScriptsToPackageFile, adjustValueInPackageFile } from '../utils/helpers';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function lcuCoreApp(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+export function lcuCoreApp(options: any): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    const templateSource = apply(url('./files/project'), [
+      template({
+        ...strings,
+        ...options,
+      }),
+      move('./'),
+    ]);
+
+    const rule = chain([
+      mergeWith(templateSource, MergeStrategy.Default)
+    ]);
+
+    return rule(tree, context);
   };
 }
