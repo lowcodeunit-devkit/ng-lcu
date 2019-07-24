73eddeb2bd89a17fbfa8465b6d8fe1c032fdeeec
mcgear <michael.gearhardt@fathym.com>
2019-07-24 08:28:11 -0600

    Adjusted for bug repro

--- src/ng-add/index.ts
+++ src/ng-add/index.ts
@@ -3,9 +3,9 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function hello(_options: any): Rule {
+export function ngAdd(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    tree.create('hello.json', JSON.stringify({ workspace: 'hey' }));
+    tree.create('ng-add.json', JSON.stringify({ workspace: 'hey' }));
     
     return tree;
   };
