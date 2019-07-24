64aa06748effccab7d76a812235210e1768c9303
mcgear <michael.gearhardt@fathym.com>
2019-07-23 22:20:00 -0600

    Make blah do something

--- src/blah/index.ts
+++ src/blah/index.ts
@@ -1,12 +1,25 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, apply, url, template, chain, mergeWith, MergeStrategy, move } from '@angular-devkit/schematics';
+import { strings } from '@angular-devkit/core';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function blah(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    _context.logger.error('Doing blah...');
+export function blah(options: any): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    context.logger.error('Doing blah...');
     
-    return tree;
+    const templateSource = apply(url('./files/project'), [
+      template({
+        ...strings,
+        ...options,
+      }),
+      move('./'),
+    ]);
+
+    const rule = chain([
+      mergeWith(templateSource, MergeStrategy.Default)
+    ]);
+
+    return rule(tree, context);
   };
 }
