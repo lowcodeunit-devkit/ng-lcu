488075539fc5a92c580fcc97d85c2f2671e47a93
ttrichar <trevor.richardson@fathym.com>
2019-08-02 15:31:32 -0600

    Changes

--- src/application/index.ts
+++ src/application/index.ts
@@ -246,7 +246,7 @@ function blankOutLibrary(options: any, context: SchematicContext, exceptModule: 
 function processInitWith(options: any, context: SchematicContext) {
   return (host: Tree) => {
     context.logger.info(`Processing Initialization for ${options.initWith}...`);
-    context.logger.info("Look Here in Application!");
+    context.logger.info("Look Here in Application!!");
     let rule: Rule = noop();
 
     switch (options.initWith) {
