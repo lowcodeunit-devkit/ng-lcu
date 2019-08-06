2a2e67cd77dacdc21072c0f45c69d5dc91ec871f
ttrichar <trevor.richardson@fathym.com>
2019-08-06 11:30:03 -0600

    Add logger

--- src/momentum/index.ts
+++ src/momentum/index.ts
@@ -12,6 +12,8 @@ export function momentumApp(options: any): Rule {
 
     var project = workspace.projects[options.project];
 
+    context.logger.info("PROJECT: ${options.project}...");
+
     context.logger.info("Look Here!");
     return chain([
       externalSchematic('ng-momentum', 'scaffold', { 
