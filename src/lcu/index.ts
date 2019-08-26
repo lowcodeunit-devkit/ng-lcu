b7258a0d9a00ea5cd1a92cff05e53c1f99823195
ttrichar <trevor.richardson@fathym.com>
2019-08-26 17:44:06 -0600

    Init

--- src/lcu/index.ts
+++ src/lcu/index.ts
@@ -114,7 +114,7 @@ export function manageBuildScripts(options: any) {
 
     packageJson.scripts['build'] = 'npm run build:common && npm run build:lcu';
 
-    packageJson.scripts['start'] = `ng serve demo --port=4210`;
+    packageJson.scripts['start'] = `ng serve demo --port=42${randomizePort}${randomizePort}`;
 
     host.overwrite('package.json', JSON.stringify(packageJson, null, '\t'));
 
@@ -151,4 +151,8 @@ export function setupOptions(host: Tree, options: any): Tree {
   options.initWith = options.initWith;
 
   return host;
+}
+
+export function randomizePort(){
+  return Math.floor(Math.random() * 10);
 }
