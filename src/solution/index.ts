81cece25cea6f9b9b1adab5ebba994dd657902c8
Robert Wise <robert.wise@fathym.com>
2019-11-14 11:39:24 -0700

    Update to name for lcu.json

--- src/solution/index.ts
+++ src/solution/index.ts
@@ -102,9 +102,10 @@ function addSolutionCapabilities(host: Tree, options: any): Tree {
 
   var lcuJson = lcuFile ? JSON.parse(lcuFile.content.toString('utf8')) : {};
 
-  let capabilityName: string = options.project + '-manager'; // TODO: Do we want users to provide this?
+  // TODO: 'options.project' is wrong name (coming back as 'common') - find the right name
+  let capabilityName: string = options.name + '-manager'; // TODO: Do we want users to provide this?
 
-  let elementName: string = lcuJson.templates.workspace + '-' + options.project + '-element';
+  let elementName: string = lcuJson.templates.workspace + '-' + options.name + '-element';
 
   lcuJson.config.solutions[capabilityName] = {
     element: elementName
